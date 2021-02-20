19a0fa94d266478bdcac68db4e10e40cfddedd42
jimin hu <jiminhu@hjimin-a02.vmware.com>
2021-02-20 09:33:31 -0800

    gw use latest DM; pipeline groovy for the first 10 clusters

--- pipeline-test.groovy
+++ pipeline-test.groovy
@@ -1,15 +1,10 @@
 #!/usr/bin/env groovy
-
-
-
 def nodes = [:]
 
-nodesByLabel('cls-10s').each {
+nodesByLabel('cls-100s').each {
   nodes[it] = { ->
     node(it) {
       stage("preparation@${it}") {
-        //println it
-        sh('hostname')
         dir('subDir') {
             checkout scm
         }
@@ -38,17 +33,19 @@ kubeadmConfigPatches:
     local:
       dataDir: "/tmp/etcd"
         ''')
-        sh('sudo chmod -R 777 /home/centos/workspace/scale_client_clusters')
-        sh('/home/centos/workspace/scale_client_clusters/kind get clusters | xargs /home/centos/workspace/scale_client_clusters/kind delete clusters')
+        sh('sudo chmod -R 777 /home/centos/workspace/slave-30s')
+        sh('/home/centos/workspace/slave-30s/kind get clusters | xargs /home/centos/workspace/slave-30s/kind delete clusters')
        
         
-        def uuid1 = Math.abs(new Random().nextInt() % 1000) + 1
-        sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name slave-30-cls-'''+ uuid1 +''' --config /home/centos/workspace/scale_client_clusters/kind.config''') 
+        def uuid1 = Math.abs(new Random().nextInt() % 10000) + 1
+        sh('''/home/centos/workspace/slave-30s/kind create cluster --name slave-100-cls-'''+ uuid1 +''' --config /home/centos/workspace/slave-30s/kind.config''')
 
+        def uuid2 = Math.abs(new Random().nextInt() % 10000) + 1
+        sh('''/home/centos/workspace/slave-30s/kind create cluster --name slave-100-cls-'''+ uuid2 + ''' --config /home/centos/workspace/slave-30s/kind.config''')
         
         sh('docker build -t 477502 -f Dockerfile .')
-        sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/workspace/scale_client_clusters:/home/centos/workspace/scale_client_clusters -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
-        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters slave-30-cls-''' + uuid1+''' --log-dir . --debug --csp-token kJfh2ZsImeLwv3AT7zGuTFTuRv8OpdIkydseLluytz3pdU6rajZBP3aHV1HQoOCW --clusters-per-tenant 1 --apps-per-cluster 17"''')
+        sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/workspace/slave-30s:/home/centos/workspace/slave-30s -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
+        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/slave-30s && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/slave-30s && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters slave-100-cls-''' + uuid1+''',slave-100-cls'''+uuid2+''' --log-dir . --debug --csp-token kJfh2ZsImeLwv3AT7zGuTFTuRv8OpdIkydseLluytz3pdU6rajZBP3aHV1HQoOCW --clusters-per-tenant 1 --apps-per-cluster 20"''')
         }
     }
   }
