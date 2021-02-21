17a42c7c9b8ce7715c541f92debea35ca2487bd8
jimin hu <jiminhu@hjimin-a02.vmware.com>
2021-02-20 19:12:51 -0800

    Debug memory leak

--- pipeline-test.groovy
+++ pipeline-test.groovy
@@ -7,9 +7,10 @@ nodesByLabel('cls-1-10').each {
       stage("preparation@${it}") {
         sh('pwd')
 
-        // dir('subDir') {
-        //     checkout scm
-        // }
+        dir('subDir') {
+            checkout scm
+        }
+
         // sh('pwd')
         // sh('lsblk')
         
@@ -39,15 +40,15 @@ nodesByLabel('cls-1-10').each {
         //sh('/home/centos/workspace/cls-1-5/kind get clusters | xargs /home/centos/workspace/cls-1-5/kind delete clusters')
        
         
-        // def uuid1 = Math.abs(new Random().nextInt() % 10000) + 1
-        // sh('''/home/centos/workspace/cls-1-5/kind create cluster --name slave-100-cls-'''+ uuid1 +''' --config /home/centos/workspace/cls-1-5/kind.config''')
+        def uuid1 = Math.abs(new Random().nextInt() % 10000) + 1
+        sh('''/home/centos/workspace/cls-1-5/kind create cluster --name slave-100-cls-'''+ uuid1 +''' --config /home/centos/workspace/cls-1-5/kind.config''')
 
         // // def uuid2 = Math.abs(new Random().nextInt() % 10000) + 1
         // // sh('''/home/centos/workspace/cls-1-5/kind create cluster --name slave-100-cls-'''+ uuid2 + ''' --config /home/centos/workspace/cls-1-5/kind.config''')
         
         // //sh('docker build -t 477502 -f Dockerfile .')
         // //sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/workspace/cls-1-5:/home/centos/workspace/cls-1-5 -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
-        // sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/cls-1-5 && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/cls-1-5 && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters slave-100-cls-''' + uuid1+''' --log-dir . --debug --csp-token P8ewTR1jva1zc8y7g3JyvXJyoit7Xodfzj7QFNnLs66YOKDCDIWSznFu4dbrrWyv --clusters-per-tenant 1 --apps-per-cluster 17"''')
+        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/cls-1-5 && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/cls-1-5 && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters slave-100-cls-''' + uuid1+''' --log-dir . --debug --csp-token P8ewTR1jva1zc8y7g3JyvXJyoit7Xodfzj7QFNnLs66YOKDCDIWSznFu4dbrrWyv --clusters-per-tenant 1 --apps-per-cluster 17"''')
         }
     }
   }
