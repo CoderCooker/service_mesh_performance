16283b49259b8cfd332842d51bdeac26db3088e9
jimin hu <jiminhu@hjimin-a02.vmware.com>
2021-02-24 12:33:19 -0800

    close memory leak

--- 11-18-pipeline.groovy
+++ 11-18-pipeline.groovy
@@ -19,30 +19,30 @@ nodesByLabel('cls-11-15').each {
             checkout scm
         }
 
-        sh('curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64')
-        sh('chmod +x kind')
-        sh('echo "" > kind.config')
-        sh('''cat <<EOF | sudo tee kind.config
----
-apiVersion: kind.x-k8s.io/v1alpha4
-kind: Cluster
-nodes: 
-  - role: control-plane
-  - role: worker
-  - role: worker
-  - role: worker
-  - role: worker
-kubeadmConfigPatches:
-- |
-  kind: ClusterConfiguration
-  metadata:
-    name: config
-  etcd:
-    local:
-      dataDir: "/tmp/etcd"
-        ''')
+//         sh('curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64')
+//         sh('chmod +x kind')
+//         sh('echo "" > kind.config')
+//         sh('''cat <<EOF | sudo tee kind.config
+// ---
+// apiVersion: kind.x-k8s.io/v1alpha4
+// kind: Cluster
+// nodes: 
+//   - role: control-plane
+//   - role: worker
+//   - role: worker
+//   - role: worker
+//   - role: worker
+// kubeadmConfigPatches:
+// - |
+//   kind: ClusterConfiguration
+//   metadata:
+//     name: config
+//   etcd:
+//     local:
+//       dataDir: "/tmp/etcd"
+//         ''')
         
-        //sh('/home/centos/workspace/cls-1-5/kind get clusters | xargs /home/centos/workspace/cls-1-5/kind delete clusters')
+        sh('/home/centos/workspace/cls-1-5/kind get clusters | xargs /home/centos/workspace/cls-1-5/kind delete clusters')
        
         
         def uuid1 = Math.abs(new Random().nextInt() % 150000000) + 1
@@ -51,7 +51,7 @@ kubeadmConfigPatches:
         // def uuid2 = Math.abs(new Random().nextInt() % 1200000) + 1
         // sh('''/home/centos/workspace/cls-1-5/kind create cluster --name cls11-15-'''+ uuid2 + ''' --config /home/centos/workspace/cls-1-5/kind.config''')
         
-        //sh('docker build -t 477502 -f Dockerfile .')
+        sh('docker build -t 477502 -f Dockerfile .')
         //sh('docker stop execution')
         //sh('docker rm execution')
         sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/workspace/cls-1-5:/home/centos/workspace/cls-1-5 -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
