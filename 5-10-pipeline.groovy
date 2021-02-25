b8cbd0b75b82b1bacb48b6943e15e66592090c3b
jimin hu <jiminhu@hjimin-a02.vmware.com>
2021-02-24 19:53:58 -0800

    close collecting gns data

--- 5-10-pipeline.groovy
+++ 5-10-pipeline.groovy
@@ -1,19 +1,19 @@
 #!/usr/bin/env groovy
 def nodes = [:]
 
-nodesByLabel('cls-12-24').each {
+nodesByLabel('cls-5-15').each {
   nodes[it] = { ->
     node(it) {
       stage("preparation@${it}") {
 
-        // sh('sudo yum install git -y')
-        // sh('sudo growpart /dev/nvme0n1 2 && sudo xfs_growfs -d /')
-        // sh('lsblk')
+        sh('sudo yum install git -y')
+        sh('sudo growpart /dev/nvme0n1 2 && sudo xfs_growfs -d /')
+        sh('lsblk')
         // sh('df -mh')
         // sh('free -mh')
 
-        // sh('sudo mkdir -p /tmp/etcd && sudo chmod -R 777 /tmp/etcd')
-        // sh('sudo chmod -R 777 /home/centos/workspace/scale_client_clusters')
+        sh('sudo mkdir -p /tmp/etcd && sudo chmod -R 777 /tmp/etcd')
+        sh('sudo chmod -R 777 /home/centos/workspace/scale_client_clusters')
 
         dir('/home/centos/workspace/scale_client_clusters') {
             checkout scm
@@ -42,25 +42,23 @@ kubeadmConfigPatches:
       dataDir: "/tmp/etcd"
         ''')
         
-        sh('/home/centos/workspace/scale_client_clusters/kind get clusters | xargs /home/centos/workspace/scale_client_clusters/kind delete clusters')
+        //sh('/home/centos/workspace/scale_client_clusters/kind get clusters | xargs /home/centos/workspace/scale_client_clusters/kind delete clusters')
        
         
         def uuid1 = Math.abs(new Random().nextInt() % 50000000) + 1
-        sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls510-'''+ uuid1 +''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
+        sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls515-'''+ uuid1 +''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
 
-        // def uuid2 = Math.abs(new Random().nextInt() % 1200000) + 1
-        // sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls510-'''+ uuid2 + ''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
+        def uuid2 = Math.abs(new Random().nextInt() % 50000000) + 1
+        sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls515-'''+ uuid2 + ''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
         
-        //sh('docker build -t 477502 -f Dockerfile .')
-        sh('docker stop execution')
-        sh('docker rm execution')
+        sh('docker build -t 477502 -f Dockerfile .')
         sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/workspace/scale_client_clusters:/home/centos/workspace/scale_client_clusters -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
         // two clusters
-        //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls510-''' + uuid1+''',cls510-'''+uuid2+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')
+        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls515-''' + uuid1+''',cls515-'''+uuid2+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')
 
         // // staging-0
-        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls1-5-''' + uuid1+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')        
-        //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls510-''' + uuid1+''' --log-dir . --debug --csp-token P8ewTR1jva1zc8y7g3JyvXJyoit7Xodfzj7QFNnLs66YOKDCDIWSznFu4dbrrWyv --clusters-per-tenant 1 --apps-per-cluster 17"''')        
+        //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls1-5-''' + uuid1+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')        
+        //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls515-''' + uuid1+''' --log-dir . --debug --csp-token P8ewTR1jva1zc8y7g3JyvXJyoit7Xodfzj7QFNnLs66YOKDCDIWSznFu4dbrrWyv --clusters-per-tenant 1 --apps-per-cluster 17"''')        
         
         }
     }
