1490e9fa5e3eee25fea9d6b5ed33ce129a112173
jimin hu <jiminhu@hjimin-a02.vmware.com>
2021-02-24 21:23:57 -0800

    close collecting data

--- 5-10-pipeline.groovy
+++ 5-10-pipeline.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def nodes = [:]
 
-nodesByLabel('cls-5-15').each {
+nodesByLabel('cls-41-50').each {
   nodes[it] = { ->
     node(it) {
       stage("preparation@${it}") {
@@ -19,42 +19,42 @@ nodesByLabel('cls-5-15').each {
             checkout scm
         }
 
-        sh('curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64')
-        sh('chmod +x kind')
-        sh('echo "" > kind.config')
-        sh('''cat <<EOF | sudo tee kind.config
----
-apiVersion: kind.x-k8s.io/v1alpha4
-kind: Cluster
-nodes: 
-  - role: control-plane
-  - role: worker
-  - role: worker
-  - role: worker
-  - role: worker
-kubeadmConfigPatches:
-- |
-  kind: ClusterConfiguration
-  metadata:
-    name: config
-  etcd:
-    local:
-      dataDir: "/tmp/etcd"
-        ''')
+//         sh('curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64')
+//         sh('chmod +x kind')
+//         sh('echo "" > kind.config')
+//         sh('''cat <<EOF | sudo tee kind.config
+// ---
+// apiVersion: kind.x-k8s.io/v1alpha4
+// kind: Cluster
+// nodes: 
+//   - role: control-plane
+//   - role: worker
+//   - role: worker
+//   - role: worker
+//   - role: worker
+// kubeadmConfigPatches:
+// - |
+//   kind: ClusterConfiguration
+//   metadata:
+//     name: config
+//   etcd:
+//     local:
+//       dataDir: "/tmp/etcd"
+//         ''')
         
         //sh('/home/centos/workspace/scale_client_clusters/kind get clusters | xargs /home/centos/workspace/scale_client_clusters/kind delete clusters')
        
         
-        def uuid1 = Math.abs(new Random().nextInt() % 50000000) + 1
-        sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls515-'''+ uuid1 +''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
+        // def uuid1 = Math.abs(new Random().nextInt() % 50000000) + 1
+        // sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls515-'''+ uuid1 +''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
 
-        def uuid2 = Math.abs(new Random().nextInt() % 50000000) + 1
-        sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls515-'''+ uuid2 + ''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
+        // def uuid2 = Math.abs(new Random().nextInt() % 50000000) + 1
+        // sh('''/home/centos/workspace/scale_client_clusters/kind create cluster --name cls515-'''+ uuid2 + ''' --config /home/centos/workspace/scale_client_clusters/kind.config''')
         
         sh('docker build -t 477502 -f Dockerfile .')
         sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/workspace/scale_client_clusters:/home/centos/workspace/scale_client_clusters -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
         // two clusters
-        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls515-''' + uuid1+''',cls515-'''+uuid2+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')
+        //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls515-''' + uuid1+''',cls515-'''+uuid2+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')
 
         // // staging-0
         //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/scale_client_clusters && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/scale_client_clusters && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls1-5-''' + uuid1+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')        
