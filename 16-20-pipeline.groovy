34ebdd07041a34ad9b02cf42192926945ee80bce
jimin hu <jiminhu@hjimin-a02.vmware.com>
2021-02-24 20:27:36 -0800

    close collecting data

--- 16-20-pipeline.groovy
+++ 16-20-pipeline.groovy
@@ -6,13 +6,13 @@ nodesByLabel('cls-16-20').each {
     node(it) {
       stage("preparation@${it}") {
 
-//         sh('sudo yum install git -y')
-//         sh('sudo growpart /dev/nvme0n1 2 && sudo xfs_growfs -d /')
+        sh('sudo yum install git -y')
+        sh('sudo growpart /dev/nvme0n1 2 && sudo xfs_growfs -d /')
 //         sh('lsblk')
 //         // sh('df -mh')
 //         // sh('free -mh')
 
-//         sh('sudo mkdir -p /tmp/etcd && sudo chmod -R 777 /tmp/etcd')
+        sh('sudo mkdir -p /tmp/etcd && sudo chmod -R 777 /tmp/etcd')
         sh('sudo chmod -R 777 /home/centos/workspace/slave-30s')
 
         dir('/home/centos/workspace/slave-30s') {
@@ -44,21 +44,19 @@ kubeadmConfigPatches:
         
         //sh('/home/centos/workspace/slave-30s/kind get clusters | xargs /home/centos/workspace/slave-30s/kind delete clusters')
         
-        def uuid1 = Math.abs(new Random().nextInt() % 3000000) + 1
-        sh('''/home/centos/workspace/slave-30s/kind create cluster --name cls16-20'''+ uuid1 +''' --config /home/centos/workspace/slave-30s/kind.config''')
+        def uuid1 = Math.abs(new Random().nextInt() % 30000000) + 1
+        sh('''/home/centos/workspace/slave-30s/kind create cluster --name cls26-30'''+ uuid1 +''' --config /home/centos/workspace/slave-30s/kind.config''')
 
-        // def uuid2 = Math.abs(new Random().nextInt() % 1200000) + 1
-        // sh('''/home/centos/workspace/slave-30s/kind create cluster --name cls16-20'''+ uuid2 + ''' --config /home/centos/workspace/slave-30s/kind.config''')
+        def uuid2 = Math.abs(new Random().nextInt() % 30000000) + 1
+        sh('''/home/centos/workspace/slave-30s/kind create cluster --name cls26-30'''+ uuid2 + ''' --config /home/centos/workspace/slave-30s/kind.config''')
         
-        //sh('docker build -t 477502 -f Dockerfile .')
-        sh('docker stop execution')
-        sh('docker rm execution')
+        sh('docker build -t 477502 -f Dockerfile .')
         sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/workspace/slave-30s:/home/centos/workspace/slave-30s -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
         // two clusters
-        //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/slave-30s && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/slave-30s && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls16-20''' + uuid1+''',cls16-20'''+uuid2+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')
+        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/slave-30s && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/slave-30s && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls26-30''' + uuid1+''',cls26-30'''+uuid2+''' --log-dir . --debug --csp-token OgyamW2imj6L2CKRq7RcLj4ag9S2AZXrRdBCNj02XY11r8daNXWIY9egDi9bMFw6 --clusters-per-tenant 1 --apps-per-cluster 17"''')
 
         // // staging-0
-        sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/slave-30s && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/slave-30s && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls16-20''' + uuid1+''' --log-dir . --debug --csp-token P8ewTR1jva1zc8y7g3JyvXJyoit7Xodfzj7QFNnLs66YOKDCDIWSznFu4dbrrWyv --clusters-per-tenant 1 --apps-per-cluster 17"''')        
+        //sh('''docker exec -i execution /bin/bash -c "cd /home/centos/workspace/slave-30s && export ONBOARD=true && export WORKSPACE=/home/centos/workspace/slave-30s && python3.7 library/pyfra.py --tests-dir setup/client_cluster --cluster-type kind --clusters cls26-30''' + uuid1+''' --log-dir . --debug --csp-token P8ewTR1jva1zc8y7g3JyvXJyoit7Xodfzj7QFNnLs66YOKDCDIWSznFu4dbrrWyv --clusters-per-tenant 1 --apps-per-cluster 17"''')        
         
         }
     }
