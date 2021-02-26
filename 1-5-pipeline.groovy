361534e311968286123c00dd0399ef414d3fa05a
jimin hu <jiminhu@hjimin-a02.vmware.com>
2021-02-25 16:04:35 -0800

    locate memory leak objects

--- 1-5-pipeline.groovy
+++ 1-5-pipeline.groovy
@@ -7,7 +7,7 @@ nodesByLabel('cls-1-10').each {
       stage("preparation@${it}") {
 
         sh('sudo yum install git -y')
-        sh('sudo growpart /dev/nvme0n1 2 && sudo xfs_growfs -d /')
+        //sh('sudo growpart /dev/nvme0n1 2 && sudo xfs_growfs -d /')
         sh('lsblk')
         // sh('sudo mkdir -p /tmp/etcd && sudo chmod -R 777 /tmp/etcd')
 
@@ -39,7 +39,7 @@ kubeadmConfigPatches:
       dataDir: "/tmp/etcd"
         ''')
         sh('sudo chmod -R 777 /home/centos/workspace/exhaust-master')
-        //sh('/home/centos/workspace/exhaust-master/kind get clusters | xargs /home/centos/workspace/exhaust-master/kind delete clusters')
+        sh('/home/centos/workspace/exhaust-master/kind get clusters | xargs /home/centos/workspace/exhaust-master/kind delete clusters')
        
         
         def uuid1 = Math.abs(new Random().nextInt() % 500000) + 1
